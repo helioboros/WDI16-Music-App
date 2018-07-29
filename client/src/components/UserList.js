eedd43f4afe2b07caa989c169d29d119b89c4117
helioboros <felinewraith@gmail.com>
2018-07-29 18:02:38 -0400

    removing semantic UI

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -2,20 +2,16 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import { Card, Image } from 'semantic-ui-react'
 
 const UserHeader = styled.h1`
   background-color: salmon;
 `
 
-const CardStyle = styled(Card)`
-  &&&& {
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-  :hover {
-    box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-  }
-}
+const UserCard = styled.div`
+`
+const UserCardName = styled.div`
+`
+const UserCardImage = styled.div`
 `
 
 class UserList extends Component {
@@ -40,15 +36,14 @@ class UserList extends Component {
   render() {
     const usersList = this.state.users.map((user) => {
       return (
-        <CardStyle key={user.id}>
-          <Card.Header>
+        <UserCard key={user.id}>
+          <UserCardName>
             <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </Card.Header>
-          <Card.Content>
-            <Image avatar src={user.photo_url} />
-            {/* {user.nationality} */}
-          </Card.Content>
-        </CardStyle>
+          </UserCardName>
+          <UserCardImage>
+            <img src={user.photo_url} width='100px' height='100px' />
+          </UserCardImage>
+        </UserCard>
       )
     })
 
@@ -57,9 +52,9 @@ class UserList extends Component {
         <UserHeader>
           User Index
         </UserHeader>
-        <Card.Group centered>
+        <div>
           {usersList}
-        </Card.Group>
+        </div>
       </div>
     )
   }
